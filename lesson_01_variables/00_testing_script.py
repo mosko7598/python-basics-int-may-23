eeded585840f9db4d34ae879b56ac55f6117d3a4
shahar <shaharr76ac@gmail.com>
2023-07-20 19:03:43 +0300

    Variabes lesson part 1

---
+++ lesson_01_variables/00_testing_script.py
@@ -0,0 +1,13 @@
+a = 10
+print(a)
+
+print(type(a))
+
+
+# a = 10
+# 1. 10 -> 2. a -> 3. a = 10
+
+
+# a = "shahar"
+# 1. "shahar" -> 2. a -> 3. a = "shahar"
+
---
+++ lesson_01_variables/01_variables_basics.py
@@ -0,0 +1,23 @@
+name = "shahar"
+_last_name = 'Moskovich'
+# 1age = 25
+
+age = 35
+Age = 50
+
+print(age)
+print(Age)
+
+my_name = "shahar"
+my_fill_name = "shahar Moskovich"
+
+# b1 b2 b3 - not recommended for usage
+
+age_coming_from_a_from = 45
+
+# false = 35 - keyword
+
+# aheOfEmpires -> CamelCase
+# AgeOfEmpires -> Pascal Case
+# age_of_empires -> Snake Case <--RECOMMEDED
+# Age_Of_Empires -> SnakeCase

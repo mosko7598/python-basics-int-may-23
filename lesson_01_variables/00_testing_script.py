7783c1eb8318bd00dc45d13d0ebd030196a101d4
shahar <shaharr76ac@gmail.com>
2023-07-20 20:24:01 +0300

    Variabes lesson part 1

--- lesson_01_variables/00_testing_script.py
+++ lesson_01_variables/00_testing_script.py
@@ -1,13 +1,25 @@
 a = 10
 print(a)
-
 print(type(a))
 
+a = "Shahar"
+print(a)
+print(type(a))
 
-# a = 10
-# 1. 10 -> 2. a -> 3. a = 10
+a = 10.2
+print(a)
+print(type(a))
+
+# a = 10               a  -> 10014567fe
+# 1. 10 ->  2. a  -> 3. a = 10
 
 
-# a = "shahar"
-# 1. "shahar" -> 2. a -> 3. a = "shahar"
+# a = "Alex"                 a  ->  243543feda
+# 1. "Alex"  ->  2. a ->  3. a = "Alex"
 
+
+my_list = [1, "shahar", 2, "bla bla"]
+print(my_list)
+print(type(my_list))
+
+print(type("shahar"))
---
+++ lesson_01_variables/03_numbers.py
@@ -0,0 +1,13 @@
+int_1 = 56
+int_2 = 5465451365
+int_3 = -5435
+
+float_1 = 1.45
+float_2 = -1.45
+float_3 = 35e3
+
+print(float_3)
+
+complex(float_3)
+
+complex_1 = 5j
